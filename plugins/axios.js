c55cba5a9a0d17b52c136ab4f42dc2ca4c8f841d
AmmarM0hammed <108635464+AmmarM0hammed@users.noreply.github.com>
2024-12-07 03:49:08 +0300

    asd

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,6 +2,7 @@ import axios from "axios"
 export default defineNuxtPlugin( () =>{
 
     axios.defaults.baseURL = "http://185.218.125.120:6901/api/";
+    axios.defaults.withCredentials = true; // This ensures cookies or other credentials are sent with requests
 
     axios.interceptors.request.use((config) =>{
         const userStore = useUserStore();
